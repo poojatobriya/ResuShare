965e22d956cde328aa7e7bb8bc9a70a0ae87e4f8
Pooja <poojatobriya@gmail.com>
2025-02-18 15:31:44 +0530

    implemented signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,99 +1,135 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import './Signup.css';
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
 
 const Signup = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmpassword, setConfirmpassword] = useState('');
-    const [errors, setErrors] = useState({});
-
-    const validate = () => {
-        let errors = {};
-        if (!email) {
-            errors.email = "Required";
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
-            errors.email = "Invalid email address";
+   let navigate = useNavigate();
+    const validate = values => {
+        const errors = {};
+        if (!values.name) {
+            errors.name = 'Required';
+        } else if (values.name.length > 20) {
+            errors.name = 'Must be 20 characters or less';
         }
-        if (!password) {
-            errors.password = "Required";
-        } else if (password.length > 25) {
-            errors.password = "Must be 25 characters or less";
-        } else if (password.length < 5) {
-            errors.password = "Must be 5 characters or more";
+        else if (values.name.length < 3) {
+            errors.name = 'Must be 3 characters or more';
         }
-        if (!confirmpassword) {
-            errors.confirmpassword = "Required";
-        } else if (password !== confirmpassword) {
-            errors.confirmpassword = "Password did not match";
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+            errors.email = 'Invalid email address';
         }
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
+        if (!values.password) {
+            errors.password = 'Required';
+        } else if (values.password.length > 25) {
+            errors.password = 'Must be 25 characters or less';
+        }
+        else if (values.password.length < 5) {
+            errors.password = 'Must be 5 characters or more';
+        }
+        if (!values.confirmPassword) {
+            errors.confirmPassword = 'Required';
+        }
+        else if (values.password !== values.confirmPassword) {
+            errors.confirmPassword = 'Password did not match';
+        }
+        return errors;
     };
-
-    // const handleSubmit = async () => {
-    //     console.log("signup hogya h");
-    //     // Your signup logic here
-    // };
-
-    const handleSubmit = async () => {
-        if (validate()) {
-            try {
-                const response = await axios.post('http://localhost:5000/signup', { email, password });
-                alert('Signup successful');
-            } catch (error) {
-                alert('Error during signup');
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirmPassword: '',
+            name: '',
+        },
+        validate,
+        onSubmit: async (values) => {
+            let response = await fetch('http://localhost:5500/api/auth/createuser', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values)
+            })
+            response = await response.json();
+            console.log(response);
+            if (response.success) {
+                localStorage.setItem("token", response.authToken);
+                navigate("/home");
             }
-        }
-    };
+            else {
+                alert("Invalid Credentials")
+            }
+        },
+    });
 
     return (
-        <div className="login-container">
-            <form onSubmit={(e) => { e.preventDefault(); if (validate()) { handleSubmit(e); } }} className="login-form">
-                <div className="mb-3">
-                    <p>Enter Email :</p>
-                    <input
-                        type="email"
-                        id="email"
-                        className="signup form-control"
-                        name="email"
-                        placeholder="Enter email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    {errors.email && <p className="error">{errors.email}</p>}
+        <div className="row signup-margin">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+                <div className='signup-form'>
+                    <h3>Welcome aboard!</h3>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="name" className='signup form-label'>Name</label>
+                            <input
+                                id="name"
+                                className='signup form-control'
+                                name="name"
+                                type="text"
+                                onChange={formik.handleChange}
+                                value={formik.values.name}
+                                placeholder='Enter full name'
+                            />
+                            {formik.touched.name && formik.errors.name ? <div className='errorMessage'>{formik.errors.name}</div> : null}
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className='signup form-label'>Email Address</label>
+                            <input
+                                id="email"
+                                className='signup form-control'
+                                name="email"
+                                type="email"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                placeholder='Enter email'
+                            />
+                            {formik.touched.email && formik.errors.email ? <div className='errorMessage'>{formik.errors.email}</div> : null}
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className='signup form-label'>Password</label>
+                            <input
+                                id="password"
+                                className='signup form-control'
+                                name="password"
+                                type="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                                placeholder='Enter Password'
+                            />
+                            {formik.touched.password && formik.errors.password ? <div className='errorMessage'>{formik.errors.password}</div> : null}
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="confirmPassword" className='signup form-label'>Confirm Password</label>
+                            <input
+                                id="confirmPassword"
+                                className='signup form-control'
+                                name="confirmPassword"
+                                type="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.confirmPassword}
+                                placeholder='Confirm password'
+                            />
+                            {formik.touched.confirmPassword && formik.errors.confirmPassword ? <div className='errorMessage'>{formik.errors.confirmPassword}</div> : null}
+                        </div>
+                        <button type="submit" className='btn btn-purple btn-primary signup my-2'>Submit</button>
+                    </form>
                 </div>
-
-                <div className="mb-3">
-                    <p>Enter Password :</p>
-                    <input
-                        type="password"
-                        id="password"
-                        className="signup form-control"
-                        name="password"
-                        placeholder="Enter Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {errors.password && <p className="error">{errors.password}</p>}
-                </div>
-
-                <div className="mb-3">
-                    <p>Confirm Password :</p>
-                    <input
-                        type="password"
-                        id="confirmpassword"
-                        className="signup form-control"
-                        name="confirmPassword"
-                        placeholder="Confirm password"
-                        value={confirmpassword}
-                        onChange={(e) => setConfirmpassword(e.target.value)}
-                    />
-                    {errors.confirmpassword && <p className="error">{errors.confirmpassword}</p>}
-                </div>
-
-                <button type="submit" className="login-button">Signup</button>
-            </form>
+            </div>
+            <div className="col-md-4"></div>
         </div>
     );
 };
